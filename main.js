b4585ac86d44083f8f512b89925195a07d2ee979
webnerd-sohail <sohailshaikh7875@gmail.com>
2024-09-12 23:14:27 +0530

    stage one modeling completed successfully

--- main.js
+++ main.js
@@ -1,61 +1,69 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { rgbShift } from 'three/webgpu';
 
-//create a scene with background #f0f0f0
+const canvas = document.getElementById('canvas');
+
+//create a scene
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#f0f0f0');
+scene.background = new THREE.Color('#000000');
 
-//Create a camera
+//camera 
 
-const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.z = 5;
+camera.position.z = 4;
 
-// create a cube object
+//create a object
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshLambertMaterial({ color: "#00d5ff", emissive : "#00d5ff"});
+const geometry = new THREE.DodecahedronGeometry(1.1, 0);
+const material = new THREE.MeshLambertMaterial({ color: "#fdff00", emissive : "#004444"});
 
-const cube = new THREE.Mesh(geometry, material);
+const decahedron = new THREE.Mesh(geometry, material);
 
-scene.add(cube);
+const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: "#00d5ff", emissive : "#00d5ff"});
 
-// add lightning 
 
-const light  =  new THREE.DirectionalLight(0x9CDBA6, 10);
-light.position.set = (1,1,1);
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -2;
 
+scene.add(box);
+scene.add(decahedron);
+
+
+//light 
+
+const light = new THREE.SpotLight( 0x056769, 105, 45.9, 90, 1, 1);
+
+light.position.set( 1.1,1,2 );
 scene.add(light);
+light.castShadow = true;
 
 
-//seetup a renderer
+const renderer = new THREE.WebGLRenderer({canvas});
 
-const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setPixelRatio(window.devicePixelRatio);
 
+// ADD orbit controls
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
 
-// lets animate the scene \
+//add animation 
 
-function animate()
-{
-    
+function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    decahedron.rotation.x += 0.01;
+    decahedron.rotation.y += 0.01;
+    box.rotation.y += 0.005;
     renderer.render(scene, camera);
-
-
+    controls.update();
+    renderer.render( scene, camera );
 }
 
 animate();
-
-
-// i did successfully animation of the cube. 
-
-// add controls
-
